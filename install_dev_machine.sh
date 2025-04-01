f4bab17c71a4fb0793eafb25e1c4a4f69ffcfa98
Urs Lang <urs.lang@nemundo.ch>
2025-04-01 19:18:13 +0200

    first linux commit

---
+++ install_dev_machine.sh
@@ -0,0 +1,12 @@
+echo "Installation"
+
+sudo apt update
+sudo apt upgrade
+sudo apt-get dist-upgrade -y
+sudo apt install git -y
+
+sudo snap install pycharm-community --classic
+sudo snap install signal-desktop
+sudo snap install notepad-plus-plus
+sudo snap install code --classic
+
